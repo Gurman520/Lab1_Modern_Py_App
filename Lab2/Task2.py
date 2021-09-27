42e24b4b041dc5dd7fe51448a6bb9ced1f3f854c
Romanidzs <romanidzs@gmail.com>
2021-09-28 00:32:38 +0700

    Task2 - complete

---
+++ Lab2/Task2.py
@@ -0,0 +1,38 @@
+import sys
+import os.path
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from UI2_ui import Ui_MainWindow
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.open_file()
+        self.pushButton.clicked.connect(self.run)
+
+    def run(self):
+        f = open('Log.txt', 'a')
+        date = self.calendarWidget.selectedDate()
+        data = self.timeEdit.text() + " " + date.toString('dd-MM-yyyy') + " " + self.lineEdit.text()
+        self.listWidget.isSortingEnabled()
+        self.listWidget.insertItem(1, data+"\n")
+        f.write(data+"\n")
+        f.close()
+
+    def open_file(self):
+        if os.path.isfile('Log.txt'):
+            f = open('Log.txt', 'r')
+            data = f.readlines()
+            print(data)
+            for i in range(len(data)):
+                self.listWidget.insertItem(1, data[i])
+            self.listWidget.isSortingEnabled()
+            f.close()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
